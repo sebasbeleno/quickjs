778fbf9795cc2ecd516da1806cd8d97bdfcff42e
Sebastian Herrera <sebasbeleno15@gmail.com>
2024-11-24 21:23:37 -0500

    sidebar file item dropdown menu

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { File as FileIcon } from 'lucide-react';
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar';
+import { File as FileIcon, MoreHorizontal } from 'lucide-react';
+import { SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar';
 import { File, setCurrentFileId } from '@/store/slices/filesSlice';
 import { useDispatch } from 'react-redux';
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+} from './ui/dropdown-menu';
 
 interface FileListProps {
     files: File[];
@@ -32,6 +39,39 @@ const FileList: React.FC<FileListProps> = ({ files, currentFileId }) => {
                                 <span>{name}</span>
                             </div>
                         </SidebarMenuButton>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuAction>
+                                    <MoreHorizontal />
+                                </SidebarMenuAction>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>
+                                    File actions
+                                </DropdownMenuLabel>
+                                <DropdownMenuItem disabled>
+                                    <div className='flex justify-between w-full items-center'>
+                                        <span>Share</span>
+                                        <span className='text-[10px] bg-yellow-200 rounded-md px-2 text-yellow-950'>Soon</span>
+                                    </div>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className='cursor-pointer'>
+                                    <span>
+                                        Rename
+                                    </span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className='cursor-pointer'>
+                                    <span>
+                                        Duplicate
+                                    </span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className='cursor-pointer'>
+                                    <span className='text-red-500'>
+                                        Delete
+                                    </span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </SidebarMenuItem>
                 );
             })}
